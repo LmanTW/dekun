2fdeddb4b11b046025cb5a7194cba287c4c9ba2c
LmanTW <lmantw2009@gmail.com>
2025-09-09 16:18:01 +0800

    fix: I love Python package managing

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+# Python is so stupid, why is this file even required???
+# No idea why everybody just choose Python as the LLM's langauge, it's not like javascript is not that much harder.
+# Even javascript (which is honest still not that great) is still way better than python, the syntax, the package manager, everything. It's jsut better.
